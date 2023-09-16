7dc22a9b8f85842d3dd39158c8453b5ed646f6af
kitzoyan <110635359+kitzoyan@users.noreply.github.com>
2023-09-16 00:21:53 -0400

    Cleaned up tankDrive.java, new untested proportional balance
    For the reason the swerve base would require auto balance, I have created a new method using proportionality to modify its speed. There is only deacceleration.

--- src/main/java/frc/robot/subsystems/tankDrive.java
+++ src/main/java/frc/robot/subsystems/tankDrive.java
@@ -4,13 +4,14 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
+import frc.robot.Robot;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
@@ -19,45 +20,52 @@ import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import com.revrobotics.RelativeEncoder;
-import frc.robot.Robot;
-
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers.Default;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
-import frc.robot.RobotContainer;
+import com.revrobotics.RelativeEncoder;
 
 
 public class tankDrive extends SubsystemBase {
-
 
   public CANSparkMax m_leftFrontMotor = RobotContainer.leftFrontMotor;
   public CANSparkMax m_leftBackMotor = RobotContainer.leftRearMotor;
   public static CANSparkMax m_rightFrontMotor = RobotContainer.rightFrontMotor;
   public CANSparkMax m_rightBackMotor = RobotContainer.rightRearMotor;
   
-  
   MotorControllerGroup leftMotors = new MotorControllerGroup(m_leftBackMotor, m_leftFrontMotor);
   MotorControllerGroup rightMotors = new MotorControllerGroup(m_rightBackMotor, m_rightFrontMotor);
+
+  public static RelativeEncoder frEncoder = RobotContainer.rightFrontMotor.getEncoder();
   
-  
-  //m_Drive = new DifferentialDrive(leftMotors, rightMotors);
-  public boolean centerPassed = false;
-  private double starterSpeed = 0.035;
+  private double driveSpeed = 0.035;
   private double degree = 4;
   private double prevVal;
-  private boolean addSpeed = false;
   private double speedLimit = 0.15;
-  public static RelativeEncoder frEncoder = RobotContainer.rightFrontMotor.getEncoder();
   public static double autoChargeInches; //Community 54", ramp 14", cStation top 76"
   private double inPerEncoder = 2.289; // 19 inches per 8.3 encoder value, one wheel rotation
   public static double distance = 0;
-  private boolean logic2 = false;
   public static double additionalSpeed = 0.0002;
-  private boolean logic3 = false;
-  
+  private final double START_SPEED = driveSpeed;
+  private int polarity = 1;
 
-  /** Creates a new ExampleSubsystem. */
+  private boolean logic2 = false;  
+  private boolean logic3 = false;
+  private boolean addSpeed = false;
+  public boolean centerPassed = false;
+
+  //This is for proportional code *futuer use for swerve*
+  private final double INIT_SPEED = 0.035;
+  private double activeSpeed = INIT_SPEED; // The speed that is actually ran
+  private double proportionalSpeed = 0;
+  private final double DEGREE_RANGE = 4;
+  private final double SET_POSITION = 97; //inches
+  private final double K_CONSTANT = 0;
+  private double difference = 0;
+  private final double MAX_SPEED = 1;
+  private final double MIN_SPEED = 0.2; //example values
+  
+  /* Creates a new ExampleSubsystem. */
   public tankDrive() {
     m_rightBackMotor.setInverted(true);
     m_rightFrontMotor.setInverted(true);
@@ -65,26 +73,15 @@ public class tankDrive extends SubsystemBase {
   }
 
   public void tankDrive(double leftSpeed, double rightSpeed){
-    SmartDashboard.putNumber("B- Speed", starterSpeed);
-    //SmartDashboard.putBoolean("B- Adding Speed", addSpeed);
+    SmartDashboard.putNumber("B- Speed", driveSpeed);
     SmartDashboard.putBoolean("B- Passed Center", centerPassed);
-    //m_Drive.tankDrive(leftSpeed, rightSpeed);
     m_rightFrontMotor.set(rightSpeed);
     m_leftFrontMotor.set(leftSpeed);
     m_leftBackMotor.set(leftSpeed);
     m_rightBackMotor.set(rightSpeed);
-    //leftMotors.set(leftSpeed);
   }
 
   public void straight(double speed) {
-    // straight(starterSpeed); 
-    //   if (starterSpeed < speedLimit) { // if wheels have not moved, increase speed
-    //     starterSpeed += 0.0002;
-    //     addSpeed = true;
-        
-    //   } else {
-    //     addSpeed = false;
-    //   }
     tankDrive(speed, speed);
   }
 
@@ -100,7 +97,6 @@ public class tankDrive extends SubsystemBase {
       m_rightBackMotor.setIdleMode(CANSparkMax.IdleMode.kCoast);
       m_leftBackMotor.setIdleMode(CANSparkMax.IdleMode.kCoast);
     }
-    
   }
 
   public void pivot(double speed, boolean left) {
@@ -113,7 +109,6 @@ public class tankDrive extends SubsystemBase {
 
   public void stop() {
       tankDrive(0, 0);
-      //System.out.println("DPAD UP");
   }
 
   public void balance(double pitch) {
@@ -123,83 +118,72 @@ public class tankDrive extends SubsystemBase {
     } else {
       //additionalSpeed = 0.0002;
     }
-    
-    //frontRight.setIdleMode(CANSparkMax.IdleMode.kBrake);
-    // For future note, intially start with high power to overcome
-    // static friction, then lower power to move at constant slow velocity
+      // For future note, intially start with high power to overcome
+      // static friction, then lower power to move at constant slow velocity
+      //Currently, steadily increase speed until it start moves
+    if(Math.abs(pitch) > degree) {
+      polarity = (pitch > degree ? 1 : -1);
+      straight(driveSpeed * polarity); 
 
-    //Currently, steadily increase speed until it start moves
-    
-    if(pitch > degree) {
-      straight(starterSpeed); 
-      if (starterSpeed < speedLimit) { // if wheels have not moved, increase speed
-        starterSpeed += additionalSpeed;
-        addSpeed = true;
-        
-      } else {
-        addSpeed = false;
-      }
-      centerPassed = false;
-      
-      SmartDashboard.putBoolean("B- Moving!", true);
-    } else if (pitch < -degree) {
-      straight(-starterSpeed);
-      if (starterSpeed < speedLimit) { // if wheels have not moved, increase speed
-        starterSpeed += additionalSpeed;
+      if (driveSpeed < speedLimit) { // if wheels have not moved, increase speed
+        driveSpeed += additionalSpeed;
         addSpeed = true;
       } else {
         addSpeed = false;
       }
+
+      centerPassed = false;      
       SmartDashboard.putBoolean("B- Moving!", true);
-      centerPassed = false;
     } else {
-      starterSpeed = 0.035;
+      driveSpeed = START_SPEED;
       brake(true);
       stop();
-      SmartDashboard.putBoolean("B- Moving!", false);
       addSpeed = false;
       centerPassed = true;
+      SmartDashboard.putBoolean("B- Moving!", false);      
+    }
+  }
+
+  public void proportionalBalance(double pitch, double roll, double yaw, double activePosition) {
+    brake(true);
+      //It is possible to assign yaw = 0 at beginning of the game, and know when if the robot
+      //drives onto the charging station tilted if yaw is not 0, or roll is not 0 when pitch is tilted
+
+      //Mode one, realtive-distance dependent code
+      //only works on charging staion because it's using gryo and proptionality
+    straight(activeSpeed);
+    difference = SET_POSITION - activePosition; //If behind SP, difference is (+) and pass SP is (-)
+    proportionalSpeed = INIT_SPEED * difference*K_CONSTANT; //This calculation is always running, is applied when necessary
+    
+    if (pitch > DEGREE_RANGE && pitch < -DEGREE_RANGE) {         
+      activeSpeed = proportionalSpeed;
+      activeSpeed = (activeSpeed > MAX_SPEED ? MAX_SPEED : proportionalSpeed);
+      activeSpeed = (activeSpeed < MIN_SPEED ? MIN_SPEED : proportionalSpeed);
+        //activeSpeed must never start increasing from 0 velocity, it won't be strong enough
+        //there must be a minimum moving speed, as executed above           
+    } else {
+      activeSpeed = 0;
     }
   }
 
   public void autoChargeStation() {
-    //SmartDashboard.putData(leftMotors);
-        // if (!logic2) {
-        //    //in inches
-        // } else {
-        //   if (!logic3) {
-        //       //autoChargeInches = 90;
-        //   } else {
-        //      //to stop distance tracking
-        //   }
-          
-        // }
-
         distance = frEncoder.getPosition() * inPerEncoder;
         
-        if (Math.abs(distance) < 175 && !logic2) {
-          if (Math.abs(distance) < 90) {
+        if (Math.abs(distance) < 175 && !logic2) { //175 pass and over charge station
+          if (Math.abs(distance) < 90) { //High speed until near mid of charge
             straight(-0.5);
           } else {
-            straight(-0.2);
-          }
-          //System.out.println("Running");
-            
+            straight(-0.2); //Don't want bot flying off the station
+          }            
       
         } else {
-          logic2 = true;
-          if (!logic3 && Math.abs(distance) > autoChargeInches) {
-            straight(0.5);
-
-            System.out.println(distance);
+          logic2 = true; //Next logic acivated
+          if (!logic3 && Math.abs(distance) > autoChargeInches) { //102, onto charge station from mid field side
+            straight(0.5); //high power to get on
           } else {
             logic3 = true;
-            balance(Robot.pitch);
-            System.out.println("balancing!");
+            balance(Robot.pitch); //auto balance
           }
-          
-            
-            //System.out.println("Balance");
         }
         
   }
